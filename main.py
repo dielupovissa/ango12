f7511a56fec4b860ed8a45f61660269dda44c11b
dielupovissa <dielupovissa@gmail.com>
2022-05-29 08:03:34 +0100

    Imprime uma ficha

--- main.py
+++ main.py
@@ -23,34 +23,39 @@ def text(np, nj):
 
 def cumSum(x):
     
-    return [[cSum(x,i,j)for i in range(3)] for j in range(1)]
+    return [[cSum(x,i,j)for i in range(3)] for j in range(13)]
 def cSum(x,i,j):
     sum = 0
     if i == 0: return x[j][0]
     for n in range(i+1):
         sum = sum + x[j][i-n]
     return sum
-    
-x = [[intProb(text(np, nj)) for np in range(3)] for nj in range(1)]
 
-sx = cumSum(x)
-
-def resJog(sx):
+def resJog(sx,jg):
     cum1 =0
     cum2 = 0
     cum3 = 0
     for i in range(100):
         t = random.random()
-        if t < sx[0][0]:
+        if t < sx[jg][0]:
             #print('1 ')
             cum1 = cum1 +1
     
-        elif t < sx[0][1]:
+        elif t < sx[jg][1]:
             #print('x ')
             cum2 = cum2 + 1
         else:
             #print('2 ')
             cum3 = cum3 + 1
-    if cum1 > cum2 and cum1 > cum3: return cum3
-    elif cum2 > cum1 and cum2 > cum3: return cum2
-    else: return cum3
+    if cum1 > cum2 and cum1 > cum3: return '1'
+    elif cum2 > cum1 and cum2 > cum3: return 'x'
+    else: return '2'
+
+    
+x = [[intProb(text(np, nj)) for np in range(3)] for nj in range(13)]
+
+sx = cumSum(x)
+
+for i in range(13):
+    print(resJog(sx,i))
+
